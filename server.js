e602bc1183d3bc0628d2b45292e8f6ef49619787
asbubam <asbubam@smartstudy.co.kr>
2017-06-07 15:48:22 +0900

    add circle.yml and lint

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-'use strict';
-
+/* eslint no-console: "warn" */
 const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-	res.send('Hello World!\n');
+  res.send('Hello World!\n');
 });
 
 app.listen(PORT, () => {
-	console.log('hello-node app listening on port 3000!');
+  console.log('hello-node app listening on port 3000!');
 });
 
