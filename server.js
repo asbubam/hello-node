1dd30f3c637480d3552eea3bf24f7155b775e0e5
asbubam <asbubam@smartstudy.co.kr>
2017-06-07 18:05:39 +0900

    use x-ray plugin only

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /* eslint no-console: "warn" */
 const express = require('express');
-const AWSXRay =  require('aws-xray-sdk');
+const AWSXRay = require('aws-xray-sdk');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,13 +8,13 @@ const PORT = process.env.PORT || 3000;
 // Enable EC2 plugin
 AWSXRay.config([AWSXRay.plugins.EC2Plugin, AWSXRay.plugins.ECSPlugin]);
 
-app.use(AWSXRay.express.openSegment('hello-node'));
+//app.use(AWSXRay.express.openSegment('hello-node'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!\n');
 });
 
-app.use(AWSXRay.express.closeSegment());
+//app.use(AWSXRay.express.closeSegment());
 
 app.listen(PORT, () => {
   console.log('hello-node app listening on port 3000!');
