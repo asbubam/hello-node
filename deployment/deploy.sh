47b105d4844d3778c0d9b78e87a54a45eef65c96
asbubam <asbubam@smartstudy.co.kr>
2017-06-08 10:11:45 +0900

    Add env

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 configure_aws_cli(){
   aws --version
-  aws configure set default.region ap-northeast-1
+  aws configure set aws_access_key_id $aws_access_key_id
+  aws configure set aws_secret_access_key $aws_secret_access_key
+  aws configure set default.region $aws_default_region
   aws configure set default.output json
 }
 
 configure_aws_cli
-aws ecs describe-clusters
-
-
+aws ecs describe-clusters --cluster dev
