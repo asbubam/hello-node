2b2f3d67455873b38ea72508f0da79200a6eaf34
asbubam <asbubam@smartstudy.co.kr>
2017-06-08 10:41:25 +0900

    use ENV variable

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 configure_aws_cli(){
   aws --version
-  aws configure set aws_access_key_id $aws_access_key_id
-  aws configure set aws_secret_access_key $aws_secret_access_key
-  aws configure set default.region $aws_default_region
+  aws configure set default.region $AWS_DEFAULT_REGION
   aws configure set default.output json
 }
 
