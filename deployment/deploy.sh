66d7673f3ae9b91c3386acbee162026f18a679b0
asbubam <asbubam@smartstudy.co.kr>
2017-06-08 09:54:09 +0900

    Add configure_aws_cli function

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+configure_aws_cli(){
+  aws --version
+  aws configure set default.region ap-northeast-1
+  aws configure set default.output json
+}
+
+configure_aws_cli
 aws ecs describe-clusters
+
+
